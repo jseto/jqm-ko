827d1c62fcff10ec4fe977420850dff05f95010a
Josep Seto <jseto@jasvisio.com>
2013-09-07 22:01:47 +0200

    listview shows in a jqm way

--- api/locales.js
+++ api/locales.js
@@ -1,18 +1,11 @@
 
 function Locale( localeId, onDataReady ) {
 	var self = this;
-	// self.locale = {
-	// 	header_title: "Partides Actives",
-	// 	navbar_matches: "Partides",
-	// 	navbar_addMatch: "Nova Partida"
-	// };
-
 
 	$.getJSON( config.localePath + '/' + localeId + '.json')
 		.done( function( data ) {
 			self.locale = data;
 			onDataReady.call( this, data );
-//			ko.applyBindings( self.locale );
 		})
 		.fail( function() {
 			console.error( 'Locale "' + localeId +'" not loaded.' );
