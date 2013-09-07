2e8ef4e0ce2c6ea2c8bf47c702c2319c2593f74b
Josep Seto <jseto@jasvisio.com>
2013-09-07 22:04:22 +0200

    Match autocreated with ko.mapping

--- viewmodels/activeMatches.js
+++ viewmodels/activeMatches.js
@@ -1,11 +1,12 @@
 function Match( match ) {
-	this.id = ko.observable( match.id ),
+	ko.mapping.fromJS( match, {}, this );
+/*	this.id = ko.observable( match.id ),
 	this.team1 = ko.observable( match.team1 ),
 	this.team2 = ko.observable( match.team2 ),
 	this.score1 = ko.observable( match.score1 ),
 	this.score2 = ko.observable( match.score2 ),
 	this.status = ko.observable( match.status )
-}
+*/}
 
 function ActiveMatchesList( locale )  {
 	var self = this;
