23dd717a63445ee38311baba942305635c07d7d1
Josep Seto <jseto@jasvisio.com>
2013-09-12 17:59:15 +0200

    Separated header, footer and content files

--- app.js
+++ app.js
@@ -1,11 +1,103 @@
+
 var config = {
 	localePath: 'locales',
 	locale: 'ca',
 	remoteUrl: 'http://padel.jasvisio.com/api',
+	viewsPath: 'views',
+	viewModelsPath: 'viewmodels',
+	defaultHeader: 'header.html',
+	defaultFooter: 'footer.html',
 }
 
-$(document).ready(function () {
-	locale = new Locale( 'ca', function( locale ) {
-		ko.applyBindings( new ActiveMatchesList( locale ) );		
+var pagesOnDOM =[];
+
+function filePath( path, filename ) {
+	return path + '/' + filename;
+}
+
+function viewFilePath( view ) {
+	return filePath( config.viewsPath, view );
+}
+/*
+var Header = function( init ) {
+	var self = this;
+	self.path = config.defaultHeader;
+	self.options = {
+		'data-position': 'fixed',
+		'data-id': 'persistent',
+		'data-add-back-btn': true,
+		'data-fullscreen': false,
+	}
+
+	self.title: {
+		text: '',
+		options: {
+//			'data-bind': 'text: headerTitle', //ie:
+		}
+	}
+
+	self.render: function() {
+		var el = $('<div data-role="header"></div>');
+		$.each( self.options, function( key, value ) {
+			$(el).attr( key, value );
+		});
+		$(el).append( <h1></h1> );
+
+	}
+}
+*/
+function generatePage( pageid, content, header, footer ){
+	if ( $.inArray( pageid, pagesOnDOM ) == -1 ) {
+
+		if ( !header ) {
+			header = config.defaultHeader;
+		}
+
+		if ( !footer ) {
+			footer = config.defaultFooter;
+		}
+
+		var page = $( '<div ' + 'id="' + pageid + '" data-role="page"></div>' );
+
+		jQuery.ajaxSetup({async:false});
+
+		$.get( viewFilePath( header ), function( data ) {
+			page.append( data );
+		});
+
+		$.get( viewFilePath( content ), function( data ) {
+			page.append( data );
+		});
+
+		$.get( viewFilePath( footer ), function( data ) {
+			page.append( data );
+		});
+		
+		jQuery.ajaxSetup({async:true});
+
+		page.prependTo( $.mobile.pageContainer );
+		pagesOnDOM.push( pageid );
+		
+		return page;
+	}
+	return $( '#'+pageid );
+}
+
+window.locale = {};
+
+$(document).on('pageinit', '#__start__', function( event ) {
+	new Locale( 'ca', function( locale ) {
+		window.locale = locale;
+
+		$.mobile.changePage('#activeMatches');
 	});
+});
+
+$(document).bind('pagebeforechange', function(e,data){
+	if ( typeof data.toPage === 'string' ) {
+		var page = generatePage( 'activeMatches', 'activeMatches.html' );
+		ko.applyBindings( new ActiveMatchesList( window.locale ) );		
+		$.mobile.changePage( page, data.options );
+		e.preventDefault();
+	}
 });
--- viewmodels/activeMatches.js
+++ viewmodels/activeMatches.js
@@ -1,22 +1,55 @@
 function Match( match ) {
 	ko.mapping.fromJS( match, {}, this );
-/*	this.id = ko.observable( match.id ),
-	this.team1 = ko.observable( match.team1 ),
-	this.team2 = ko.observable( match.team2 ),
-	this.score1 = ko.observable( match.score1 ),
-	this.score2 = ko.observable( match.score2 ),
-	this.status = ko.observable( match.status )
-*/}
+	this.caption = ko.computed( function() {
+		return this.team1() + ' vs. ' + this.team2();
+	}, this );
+}
 
 function ActiveMatchesList( locale )  {
 	var self = this;
-	self.matches = ko.observableArray( [] );
+
+
+	// Locales for this view
 	ko.mapping.fromJS( locale, {}, this );
 
-	$.getJSON( config.remoteUrl + '/get_active_matches', function( all_data ) {
-        var mappedMatches = $.map( all_data, function( match ) { 
-        	return new Match( match ) });
-        self.matches( mappedMatches );
-	})
+/***** Data ***********/
+	self.headerTitle = ko.observable( locale.header_title );
+	self.matches = ko.observableArray( [] );
+	self.selectedMatch = ko.observable( null );
+	self.showingList = ko.computed( function() {
+		var a = self.selectedMatch();
+		var b = self.matches().length;
+		var result = a==null && b; //( self.selectedMatch() == null ) && ( self.matches().lenght );
+		return a==null;
+	}, this);
+
+
+/***** Behaviour ******/
+	self.showList = function( show ) {
+		self.showDetails( null ); //hide detail view
+
+		if ( show && ( !self.matches.lenght ) ) {
+			self.loadData();
+		}
+		self.headerTitle( locale.header_title );
+	}
+
+	self.showDetails = function( match ) {
+		if (match) {
+			self.headerTitle( match.caption() );
+		}
+		self.selectedMatch( match );
+	}
+
+	// Get the match list from RESTfull server
+	self.loadData = function() {
+		$.getJSON( config.remoteUrl + '/get_active_matches', function( all_data ) {
+	        var mappedMatches = $.map( all_data, function( match ) { 
+	        	return new Match( match ) });
+	        self.matches( mappedMatches );
+		});
+	}
+
+	self.showList( true );
 }
 
