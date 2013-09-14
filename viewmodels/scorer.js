fcc02e585f22b24ef8404e1823deee3a7ef7a152
Josep Seto <jseto@jasvisio.com>
2013-09-14 02:51:51 +0200

    Point buttons working

--- viewmodels/scorer.js
+++ viewmodels/scorer.js
@@ -1,5 +1,25 @@
 var Scorer = ViewModel.extend({
 	setMatch: function( match ) {
 		ko.mapping.fromJS( match, {}, this );
+	},
+
+	addPointTeam1: function() {
+		this.score1( this.score1()+1 );
+	},
+
+	addPointTeam2: function() {
+		this.score2( this.score2()+1 );
+	},
+
+	substractPointTeam1: function() {
+		if ( this.score1() > 0 ) {
+			this.score1( this.score1()-1 );
+		}
+	},
+
+	substractPointTeam2: function() {
+		if ( this.score2() > 0 ) {
+			this.score2( this.score2()-1 );
+		}
 	}
 });
